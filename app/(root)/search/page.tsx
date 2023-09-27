f0da3d9ab2e64c65e7d6872f5c413bd48ae6ca9a
Walter Diego Alva <wd.alvaf@gmail.com>
2023-09-27 14:53:11 -0500

    first comment

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,50 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import Image from "next/image";
+import { profileTabs } from "@/constants";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import UserCard from "@/components/cards/UserCard";
+
+const Page = async () => {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  //Fetch users
+  const result = await fetchUsers({ userId: user.id, searchString: "" });
+
+  return (
+    <section>
+      <h1 className="head-text mb-10">Search</h1>
+
+      {/* Search Bar */}
+
+      {/* Results */}
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result">No users</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType="User"
+              />
+            ))}
+          </>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Page;
